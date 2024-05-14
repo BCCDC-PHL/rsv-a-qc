7746abd1eb09649ccb15d95641478701b12f9795
taranewman <tara.newman98@gmail.com>
2024-05-14 15:50:07 -0700

    add nextclade version and dataset tag

--- bin/append_nextclade_version.py
+++ bin/append_nextclade_version.py
@@ -1,14 +1,14 @@
 import argparse
 import csv
 
-def add_nextclade_version(input_file, output_file, nextclade_version):
+def add_nextclade_version(input_file, output_file, dataset_version, nextclade_version):
 
     with open(input_file, 'r', newline='') as f_in:
 
         reader = csv.reader(f_in, delimiter='\t')
     
         header = next(reader)
-    
+        header.append('dataset_version')
         header.append('nextclade_version')
         
         with open(output_file, 'w', newline='') as f_out:
@@ -19,17 +19,19 @@ def add_nextclade_version(input_file, output_file, nextclade_version):
             
             for row in reader:
  
+                row.append(dataset_version)
                 row.append(nextclade_version)
    
                 writer.writerow(row)
 
 def main(args):
-    add_nextclade_version(args.nextclade_tsv, args.output_name, args.nextclade_version)
+    add_nextclade_version(args.nextclade_tsv, args.output_name, args.dataset_version, args.nextclade_version)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--nextclade_tsv")
     parser.add_argument("--output_name")
+    parser.add_argument("--dataset_version")
     parser.add_argument("--nextclade_version")
     args = parser.parse_args()
     main(args)
--- modules/rsv-a-qc.nf
+++ modules/rsv-a-qc.nf
@@ -89,7 +89,8 @@ process nextclade {
 
 
   dataset_version=$(nextclade dataset list -n ${params.nextclade_dataset} --json | python3 -c "import json,sys; data=json.load(sys.stdin); print(data[0]['version']['tag'])")
-  python append_nextclade_version.py --nextclade_tsv ${run_id}_nextclade_qc.tsv --out ${run_id}_nextclade_qc.tsv --nextclade_version ${dataset_version}
+  nextclade_version=$(nextclade --version | awk '{print $2}')
+  python append_nextclade_version.py --nextclade_tsv ${run_id}_nextclade_qc.tsv --out ${run_id}_nextclade_qc.tsv --nextclade_version ${nextclade_version} --dataset_version ${dataset_version}
 
   """
 }
